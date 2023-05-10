11c747c36bfd1634f2a39cba003dbbf37906dcd9
Chrys P <chryseltoro@hotmail.com>
2023-05-10 11:05:51 -0500

    second commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+const moduleFederationConfig = withModuleFederationPlugin({
+
+    name: 'mfRegister',
+
+    exposes: {
+        './RegisterModule': './src/app/register/register.module.ts',
+    },
+
+    shared: {
+        ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    },
+
+});
+
+moduleFederationConfig.output.publicPath = 'http://localhost:4202/'
+module.exports = moduleFederationConfig;
