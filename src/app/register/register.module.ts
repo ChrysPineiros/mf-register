07eb4367f2bc48418ab0cbb9bfcebdf8f3f28f20
Paul Pineiros <paulpieiros@gmail.com>
2023-05-10 16:13:02 -0500

    add design system

--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register.component';
 import { RouterModule, Routes } from '@angular/router';
+import { LibDesignSystemModule } from '@mycne/lib-design-system';
 
 const routes: Routes = [{ path: '', component: RegisterComponent }];
 
@@ -9,7 +10,8 @@ const routes: Routes = [{ path: '', component: RegisterComponent }];
   declarations: [RegisterComponent],
   imports: [
     RouterModule.forChild(routes), 
-    CommonModule
+    CommonModule,
+    LibDesignSystemModule
   ]
 })
 export class RegisterModule { }
